90fa5c5debe61546a379adb2f1e7472db7128459
Furuya Tatsuma <furuya.tatsuma704@mail.kyutech.jp>
2024-12-29 01:13:53 +0900

    change color

--- frontend/src/components/Project/CreateProject.js
+++ frontend/src/components/Project/CreateProject.js
@@ -130,6 +130,7 @@ const CreateProject = () => {
               <button
                 onClick={handleSubmit}
                 className="flex min-w-[84px] max-w-[480px] cursor-pointer items-center justify-center overflow-hidden rounded-xl h-10 px-4 bg-[#0074D9] text-white text-sm font-bold leading-normal tracking-[0.015em]"
+                style={{background: "#495867"}}
               >
                 <span className="truncate">Create</span>
               </button>
